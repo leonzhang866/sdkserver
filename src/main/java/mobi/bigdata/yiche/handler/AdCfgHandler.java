7ba9ba16de853e5c66924c3174a6fe9662c8b407
张亮 <zhangliang3@yiche.com>
2017-04-10 10:34:06 +0800

    first commit

--- src/main/java/mobi/bigdata/yiche/handler/AdCfgHandler.java
+++ src/main/java/mobi/bigdata/yiche/handler/AdCfgHandler.java
@@ -1,7 +1,11 @@
 package mobi.bigdata.yiche.handler;
 
+import java.util.Random;
+
+import org.json.JSONArray;
+
 import mobi.bigdata.yiche.util.QPSTool;
-import net.sf.json.JSONObject;
+import org.json.JSONObject;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
@@ -26,15 +30,67 @@ public class AdCfgHandler implements HttpHandler {
 	    
 		
 	    QPSTool.addOne();
+	    
+        Random r = new Random();
+        int adType = r.nextInt(3);
+	    
+	    
         JSONObject resultObj = new JSONObject();
-        resultObj.put("ssp_addr", sspAddr);
+        resultObj.put("code", "2000");
+        resultObj.put("msg", "success");
         
-        JSONObject returnObj = new JSONObject();
-        returnObj.put("status", "200");
-        returnObj.put("result", resultObj);
+        JSONObject contentObj = new JSONObject();
+        contentObj.put("dvid", "81ec124a6d6380e7d8fdfeef46ec3166");
+        
+        
+        JSONArray contentResultArray = new JSONArray();
+        JSONObject arrResultObj = new JSONObject();
+        arrResultObj.put("statusCode", "3000");
+        arrResultObj.put("statusMsg", "success");
+        arrResultObj.put("pid", "1035");
+        arrResultObj.put("type", String.valueOf(adType));
+        
+        
+        JSONObject adMatResultObj = new JSONObject();
+    	adMatResultObj.put("creativeid", "129046");
+    	JSONArray adMatArray = new JSONArray();
+    	adMatResultObj.put("exposureTp", "http://www.baidu.com");
+    	adMatResultObj.put("clickTp", "http://www.baidu.com");
+
+        switch (adType){
+        case 0:
+        	adMatArray.put("https://dl.ctags.cn/ad/mat/txt_pic_0/pic_text.jpg");
+        	adMatResultObj.put("title", "Das auto new arrival.");   
+            adMatResultObj.put("url", "http://www.taoche.com/volkswagen/");
+        	break;
+        case 1:
+        	adMatArray.put("https://dl.ctags.cn/ad/mat/multi_pic_1/group1.jpg");
+        	adMatArray.put("https://dl.ctags.cn/ad/mat/multi_pic_1/group2.jpg");
+        	adMatArray.put("https://dl.ctags.cn/ad/mat/multi_pic_1/group3.jpg");
+        	adMatResultObj.put("title", "Das auto new arrival.");   
+            adMatResultObj.put("url", "http://www.taoche.com/volkswagen/");
+        	break;
+        case 2:
+        	adMatArray.put("https://dl.ctags.cn/ad/mat/big_pic_2/bigpic.jpg");
+        	adMatResultObj.put("title", "Das auto new arrival."); 
+            adMatResultObj.put("url", "http://www.taoche.com/volkswagen/");
+        	break;
+        case 3:
+        	adMatArray.put("https://dl.ctags.cn/ad/mat/video_3/viedo_pic.png");
+        	adMatResultObj.put("title", "Das auto new arrival."); 
+            adMatResultObj.put("url", "https://dl.ctags.cn/ad/mat/video_3/test.mp4");
+        	break;
+        }
+        adMatResultObj.put("picUrl", adMatArray);
+        arrResultObj.put("result",adMatResultObj);
+        
+        contentResultArray.put(arrResultObj); //arr 填充一个obj
+        
+        contentObj.put("result", contentResultArray);
+        resultObj.put("content", contentObj);
         
         arg0.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-        arg0.getResponseSender().send(returnObj.toString());
+        arg0.getResponseSender().send(resultObj.toString());
 	}
 
 }
